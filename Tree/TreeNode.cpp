ea80830468194754c32e8ef897a51d8e282720b5
Carlos Soares Dias <csdias@gmail.com>
2020-10-01 23:25:21 -0300

    adding the inOrder Tree Sort

--- Tree/TreeNode.cpp
+++ Tree/TreeNode.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -72,9 +74,86 @@ struct TreeNode {
         return foundTree;
     }
 
+    vector<int> levelOrderSort(TreeNode* root) {
+        TreeNode* node = root;
+        TreeNode* left{};
+        TreeNode* right{};
+        std::queue<TreeNode*> q{};
+        vector<int> vi{};
+
+        q.push(node);
+
+        while(q.size() > 0) {
+            node = q.front();
+            vi.push_back(node->val);
+            if(node->left!=nullptr) q.push(node->left);
+            if(node->right!=nullptr) q.push(node->right);
+            q.pop();
+        }
+
+        for(int a : vi) {
+            std::cout << a << " ";
+        }
+
+        return vi;
+    }
+
+    TreeNode* inOrderSort(TreeNode* root) {
+        TreeNode* left{};
+        TreeNode* right{};
+        std::vector<int> vi{};
+
+        if (root->left == nullptr && root->right == nullptr)
+            return root;
+
+        if (root->left != nullptr) {
+            left = inOrderSort(root->left);
+        };
+
+        if (left != nullptr) 
+            vi.push_back(left->val);
+        
+            vi.push_back(root->val);
+
+        if (root->right != nullptr) {
+            right = inOrderSort(root->right);
+        };
+
+        if (right != nullptr)
+        vi.push_back(right->val);
+
+    }
+
+    vector<int> sort(TreeNode* root) {
+        TreeNode* next;
+        std::queue<TreeNode*> q{};
+        std::vector<int> vi{};
+
+        q.push(root->left);
+        while(q.size() > 0) {
+            next = q.front();
+            vi.push_back(next->val);
+            if (next->left!=nullptr) q.push(next->left);
+            if (next->right!=nullptr) q.push(next->right);
+
+            q.pop();
+        }
+        vi.push_back(root->val);
+        q.push(root->right);
+        while(q.size() > 0) {
+            next = q.front();
+            vi.push_back(next->val);
+            if (next->left != nullptr) q.push(next->left);
+            if (next->right != nullptr) q.push(next->right); 
+            q.pop();
+        }
+
+        return vi;
+    }
 };
 
 int main(int argc, char* argv[]){
+
 
     TreeNode* root = new TreeNode();
     TreeNode tree;
@@ -84,7 +163,15 @@ int main(int argc, char* argv[]){
     root = tree.add(root, 1);
     root = tree.add(root, 3);
     //[4,2,7,1,3]
+    //[3,2,1,5,4,7]
+
+    vector<int> vi = tree.levelOrderSort(root);
+     for(int a: vi) {
+         std::cout << a << " ";
+    }
+
+    //auto a = tree.levelOrderSort(root)
 
     root = tree.searchBST(root, 2);
 
-}
+};
